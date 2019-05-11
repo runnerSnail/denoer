279fd60824bf3f10d945e5982f970de4e2bdd02f
wutao <m15776849661@163.com>
2019-05-11 22:44:09 +0800

    feat: base css

--- webapp/config/webpack.config.js
+++ webapp/config/webpack.config.js
@@ -274,7 +274,9 @@ module.exports = function (webpackEnv) {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
-        utils: stancePath + '/src/utils'
+        utils: stancePath + '/src/utils',
+        components: stancePath + '/src/components',
+        '@': stancePath + '/src'
       },
       plugins: [
         // Adds support for installing with Plug'n'Play, leading to faster installs and adding
