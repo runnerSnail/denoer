bb2b0c6c772b1150a3d79bf0f220089f3ac59dd2
高铨康 <hzgaoquankang@corp.netease.com>
2019-05-21 23:58:21 +0800

    change: 增加事务控制

--- server/utils/transaction.ts
+++ server/utils/transaction.ts
@@ -3,7 +3,9 @@ export default async function  transaction(sql:string):Promise<string>{
     let client,affect;
         try {
             client = await pool.connect();
+            await client.query("BEGIN");
             affect = await client.query(sql);
+            await client.query("COMMIT");
         } catch (error) {
             await client.query("ROLLBACK");
             affect = null;
