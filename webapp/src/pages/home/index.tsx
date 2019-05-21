cfc96a98be9b4dea114e529f9770a8a53b42b8b1
wutao <m15776849661@163.com>
2019-05-21 23:13:08 +0800

    feat: publish over

--- webapp/src/pages/home/index.tsx
+++ webapp/src/pages/home/index.tsx
@@ -52,8 +52,6 @@ export default class Home extends React.Component<any, HomeState> {
   }
 
   toPublish = (path) => () => {
-    console.log('path:', path)
-    console.log('history:', this.props.history)
     this.props.history.push(path)
   }
 
@@ -85,8 +83,7 @@ export default class Home extends React.Component<any, HomeState> {
   }
 
   _listHeader = () => (
-    <div style={{ marginTop: 50, display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
-      {App({ title: 'czx' })}
+    <div className='list-header'>
       <span>全部</span>
       <span>精华</span>
       <span>分享</span>
@@ -94,80 +91,64 @@ export default class Home extends React.Component<any, HomeState> {
       <span>测试</span>
     </div>
   )
+
+  _renderContent = () => (
+    <Content>
+      <Carousel dots autoplay>
+        <div>第一张</div>
+        <div>第二张</div>
+        <div>第三张</div>
+      </Carousel>
+      <List
+        loading={this.state.loading}
+        itemLayout='vertical'
+        dataSource={this.state.dataSource}
+        renderItem={this._renderRow}
+        header={this._listHeader()}
+        pagination={{
+          // showQuickJumper: true,
+          total: 21,
+          pageSize: 8,
+          onChange: (pageNum, pageSize) => {
+            console.log('pagination onChange:', pageNum, pageSize)
+          }
+        }}
+      />
+    </Content>
+  )
+
+  _renderSider = () => (
+    <Sider
+      className='sider'
+      style={{ marginLeft: 50 }}
+    >
+      <div className='sider-wrapper'>
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+          <img src='https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png' style={{ width: 60, height: 60, borderRadius: 30 }} />
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <span style={{ fontSize: 14 }}>runSnail</span>
+            <span style={{ fontSize: 12, marginTop: 10 }}>个人说明: 人生如棋，我愿为卒，行动虽慢，可谁见我后退一步。</span>
+          </div>
+        </div>
+        <div style={{ marginTop: 10, marginBottom: 10, display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
+          <Button size='small' type='primary' onClick={this.toPublish('/publish')}>发表文章</Button>
+          <Button size='small' type='primary'>撰写文章</Button>
+          <Button size='small' type='primary'>分享资源</Button>
+        </div>
+      </div>
+      <div style={{ width: 300, height: 200, background: '#999', marginTop: 20, textAlign: 'center', lineHeight: '200px' }}>广告位</div>
+      <div style={{ width: 300, height: 200, background: '#aaa', marginTop: 20, textAlign: 'center', lineHeight: '200px' }}>广告位</div>
+      <div style={{ width: 300, height: 200, background: '#bbb', marginTop: 20, textAlign: 'center', lineHeight: '200px' }}>广告位</div>
+    </Sider>
+  )
   render () {
-    console.log('request:', request)
-    console.log('props:', this.props)
     return (
       <Page>
-        <Layout style={{
-          padding: '20px 50px',
-          marginTop: 64,
-          display: 'flex',
-          flexDirection: 'row',
-        }}
-        >
-          <Content>
-            <Carousel
-              dots
-              autoplay
-            >
-              <div>第一张</div>
-              <div>第二张</div>
-              <div>第三张</div>
-            </Carousel>
-            <List
-              loading={this.state.loading}
-              itemLayout='vertical'
-              dataSource={this.state.dataSource}
-              renderItem={this._renderRow}
-              header={this._listHeader()}
-              pagination={{
-                // showQuickJumper: true,
-                total: 21,
-                pageSize: 8,
-                onChange: (pageNum, pageSize) => {
-                  console.log('pagination onChange:', pageNum, pageSize)
-                }
-              }}
-            />
-          </Content>
-          <Sider
-            className='sider'
-            style={{
-              marginLeft: 50,
-              backgroundColor: 'rgb(240, 242, 245)'
-            }}
-          >
-            <div style={{ width: 300, height: 'auto', overflow: 'hidden', background: '#fff', color: '#000', padding: 10 }}>
-              <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                <img src='https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png' style={{ width: 60, height: 60, borderRadius: 30 }} />
-                <div style={{ display: 'flex', flexDirection: 'column' }}>
-                  <span style={{ fontSize: 14 }}>runSnail</span>
-                  <span style={{ fontSize: 12, marginTop: 10 }}>个人说明: 人生如棋，我愿为卒，行动虽慢，可谁见我后退一步。</span>
-                </div>
-              </div>
-              <div style={{ marginTop: 10, marginBottom: 10, display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
-                <Button size='small' type='primary' onClick={this.toPublish('/publish')}>发表文章</Button>
-                <Button size='small' type='primary'>撰写文章</Button>
-                <Button size='small' type='primary'>分享资源</Button>
-              </div>
-            </div>
-            <div style={{ width: 300, height: 200, background: '#999', marginTop: 20, textAlign: 'center', lineHeight: '200px' }}>广告位</div>
-            <div style={{ width: 300, height: 200, background: '#aaa', marginTop: 20, textAlign: 'center', lineHeight: '200px' }}>广告位</div>
-            <div style={{ width: 300, height: 200, background: '#bbb', marginTop: 20, textAlign: 'center', lineHeight: '200px' }}>广告位</div>
-          </Sider>
+        <Layout className='wrapper'>
+          {this._renderContent()}
+          {this._renderSider()}
         </Layout>
       </Page>
     )
   }
-}
-
-interface AppProps {
-  title: string
-}
-const App: React.SFC<AppProps> = props => {
-  console.log('props:', props)
-  return (
-    <div>{`App-${props.title}`}</div>
-  )
 }
--- webapp/src/pages/posts/publish/index.tsx
+++ webapp/src/pages/posts/publish/index.tsx
@@ -3,6 +3,9 @@ import { Select, Input, Layout, Button, Form } from 'antd'
 import Marked from 'marked'
 
 import { Page } from 'components' // Nav
+import { request } from 'utils'
+
+import styles from './style.module.sass'
 
 const { Content, Sider } = Layout
 const { TextArea } = Input
@@ -10,63 +13,103 @@ const { TextArea } = Input
 interface State {
   content: string
 }
-@Form.create()
-export default class Publish extends React.Component<any, State> {
+class Publish extends React.Component<any, State> {
 
   state = {
     content: ''
   }
 
+  componentDidMount () {
+    request('22', { article_id: 1 }, { basePath: '/api/acticle' })
+  }
+
+  handleSubmit = (e) => {
+    const { validateFields: V } = this.props.form
+    e.preventDefault()
+    V(async (err, values) => {
+      if (!err) {
+        console.log('values:', values)
+        /** 根据是否有 article_id 判断是更新文章还是创建文章 */
+        await request('create', { ...values }, { basePath: '/api/article' })
+        await request('update', { ...values }, { basePath: '/api/article' })
+      }
+      console.log('err:', err)
+    })
+  }
+
   handleChange = (event: { target }) => {
-    console.log('props:', this.props)
-    console.log('event:', event)
-    console.log('zxc:', event.target.value, typeof event.target.value)
+    // console.log('props:', this.props)
+    // console.log('event:', event)
+    // console.log('zxc:', event.target.value, typeof event.target.value)
     this.setState({ content: Marked(event.target.value) })
   }
 
-  render () {
-    return (
-      <Page>
-        <Layout style={{
-          padding: '20px 50px',
-          marginTop: 64,
-          display: 'flex',
-          flexDirection: 'row',
-        }}
-        >
-          <Content>
-            <span style={{ marginRight: 5 }}>选择板块:</span>
-            <Select
+  _renderContent = () => {
+    const { getFieldDecorator: D, getFieldsValue: G } = this.props.form
+    return (<Content>
+      <Form layout='inline'>
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+          <span style={{ marginRight: 5 }}>选择板块:</span>
+          <Form.Item>
+            {D('type', {
+              rules: [{ required: true, message: '板块类型必填' }]
+            })(<Select
               style={{ width: 100 }}
-              // size='small'
             >
               <Select.Option value='client'>客户端</Select.Option>
               <Select.Option value='server'>服务端</Select.Option>
               <Select.Option value='answer'>问答</Select.Option>
               <Select.Option value='jd'>招聘</Select.Option>
-            </Select>
-            <Input placeholder='标题字数 10 字以上' style={{ marginTop: 10 }} />
-            <div style={{ marginTop: 10, display: 'flex', flexDirection: 'row' }}>
-              <TextArea onChange={this.handleChange} style={{ height: '1000px' }} />
-              {/* <div dangerouslySetInnerHTML={{ __html: this.state.content }} style={{ flex: 1 }} /> */}
-            </div>
-          </Content>
-          <Sider
-            className='sider'
-            style={{
-              marginLeft: 50,
-              backgroundColor: 'rgb(240, 242, 245)'
-            }}
-          >
-            <div style={{ width: 300, height: 200, background: '#999', marginTop: 20, textAlign: 'center', lineHeight: '200px' }}>广告位</div>
-            <div style={{ width: 300, height: 200, background: '#aaa', marginTop: 20, textAlign: 'center', lineHeight: '200px' }}>广告位</div>
-            <div style={{ width: 300, height: 200, background: '#bbb', marginTop: 20, textAlign: 'center', lineHeight: '200px' }}>广告位</div>
-          </Sider>
+            </Select>)}
+          </Form.Item>
+        </div>
+        <Form.Item>
+          {D('title', {
+            rules: [{ required: true, message: '标题必填' }]
+          })(<Input placeholder='标题字数 10 字以上' style={{ width: 500 }} />)}
+        </Form.Item>
+        <Form.Item>
+          {D('img')(<Input placeholder='主图链接' style={{ width: 500 }} />)}
+        </Form.Item>
+        <div style={{ marginTop: 10 }}>
+          <Form.Item>
+            {D('content', {
+              rules: [{ required: true, message: '内容必填' }]
+            })(<TextArea onChange={this.handleChange} style={{ width: 800, height: '700px' }} />)}
+          </Form.Item>
+        </div>
+          {/* <div dangerouslySetInnerHTML={{ __html: this.state.content }} style={{ flex: 1 }} /> */}
+      </Form>
+  </Content>
+  )}
+
+  _renderSider = () => (
+    <Sider
+      className='sider'
+      style={{
+        marginLeft: 50,
+        backgroundColor: 'rgb(240, 242, 245)'
+      }}
+    >
+      <div style={{ width: 300, height: 200, background: '#999', marginTop: 20, textAlign: 'center', lineHeight: '200px' }}>广告位</div>
+      <div style={{ width: 300, height: 200, background: '#aaa', marginTop: 20, textAlign: 'center', lineHeight: '200px' }}>广告位</div>
+      <div style={{ width: 300, height: 200, background: '#bbb', marginTop: 20, textAlign: 'center', lineHeight: '200px' }}>广告位</div>
+    </Sider>
+  )
+
+  render () {
+    return (
+      <Page>
+        <Layout className={styles['wrapper']}>
+          {this._renderContent()}
+          {this._renderSider()}
         </Layout>
-        <div style={{ padding: '10px 50px' }}>
-        <Button size='small' type='primary'>提交</Button>
+        <div style={{ padding: '5px 50px' }}>
+          <Button size='small' type='primary' onClick={this.handleSubmit}>发布</Button>
         </div>
       </Page>
     )
   }
 }
+
+export default Form.create()(Publish)
