5bc68cc97c2a5538a8591a735501974befdf9526
高铨康 <gaomengjie_forever@163.com>
2019-06-01 00:11:51 +0800

    自动化编译之后上传静态资源

--- webapp/gulpfile.js
+++ webapp/gulpfile.js
@@ -3,23 +3,31 @@ var scss = require('gulp-scss');
 var babel = require('gulp-babel');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
 var cleanCSS = require('gulp-clean-css');
 var cssnano = require('gulp-cssnano')
 var del = require('del');
 var process = require('process');
+var htmlmin = require('gulp-htmlmin');//html压缩
 process.env.NODE_ENV = 'development';
+var replace = require('gulp-replace');
+
 var paths = {
   styles: {
-    src: './public/*.css',
+    src: './build/**/*.css',
     dest: './build'
   },
   scripts: {
-    src: './public/*.js',
+    src: './build/**/*.js',
+    dest: './build'
+  },
+  html: {
+    src: './build/*.html',
     dest: './build'
   }
 };
- 
+
+// cdn 加速域名
+var cdnPrex = '//denoer-1255609850.file.myqcloud.com'
 /* Not all tasks need to use streams, a gulpfile is just another node program
  * and you can use all packages available on npm, but it must return either a
  * Promise, a Stream or take a callback and call it
@@ -27,29 +35,45 @@ var paths = {
 function clean() {
   // You can use multiple globbing patterns as you would with `gulp.src`,
   // for example if you are using del 2.0 or above, return its promise
-  return del([ '/build/article.css' ],['/build/article.js']);
+  return del(['/build/article.css'], ['/build/article.js'], ['/build/index.html'], ['/build/article.html']);
 }
- 
+
 /*
  * Define our tasks using plain functions
  */
 function styles() {
-  return gulp.src(paths.styles.src)
+  return gulp.src(paths.styles.src,'!./build/static/css/*')
     .pipe(cleanCSS())
     // pass in options to the stream
     .pipe(cssnano())
-    .pipe(concat('article.css'))
     .pipe(gulp.dest(paths.styles.dest));
 }
- 
+
 function scripts() {
-  return gulp.src(paths.scripts.src, { sourcemaps: true })
+  return gulp.src([paths.scripts.src,'!./build/service-worker.js','!./build/static/js/*'], { sourcemaps: true })
     .pipe(babel())
     .pipe(uglify())
-    .pipe(concat('article.js'))
     .pipe(gulp.dest(paths.scripts.dest));
 }
- 
+
+function insertCDNPre() {
+  var options = {
+    removeComments: true,//清除HTML注释
+    collapseWhitespace: true,//压缩HTML
+    collapseBooleanAttributes: false,//省略布尔属性的值 <input checked="true"/> ==> <input />
+    removeEmptyAttributes: false,//删除所有空格作属性值 <input id="" /> ==> <input />
+    removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
+    removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
+    minifyJS: true,//压缩页面JS
+    minifyCSS: true//压缩页面CSS
+  };
+  return gulp.src(paths.html.src)
+    .pipe(replace(/href="/g, `href="${cdnPrex}`))
+    .pipe(replace(/src="/g, `src="${cdnPrex}`))
+    .pipe(htmlmin(options))
+    .pipe(gulp.dest('build/'));
+}
+
 function watch() {
   gulp.watch(paths.scripts.src, scripts);
   gulp.watch(paths.styles.src, styles);
@@ -58,8 +82,9 @@ function watch() {
 /*
  * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
  */
-var build = gulp.series(clean,gulp.parallel(styles, scripts));
- 
+var build = gulp.series(gulp.parallel(styles, scripts),insertCDNPre);
+
+// var build = gulp.series(gulp.parallel(insertCDNPre));
 /*
  * You can use CommonJS `exports` module notation to declare tasks
  */
