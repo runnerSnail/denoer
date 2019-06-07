38722483127225182a200bb7d114123345193521
高铨康 <gaomengjie_forever@163.com>
2019-06-08 00:24:34 +0800

    change:测试ci

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 ssh -tt $user@$ip;
 
 echo '前端打包'
-# cd webapp && npm i && npm run build
+cd webapp && npm i && npm run build
 
-# echo '前端上传cdn'
-# cd webapp && npm run deploy
+echo '前端上传cdn'
+cd webapp && npm run deploy
 
-# echo '拷贝打包后的html前端文件到template'
-# cp -r webapp/build/*.html server/template
+echo '拷贝打包后的html前端文件到template'
+cp -r webapp/build/*.html server/template
 
-# echo '启动server'
+echo '启动server'
